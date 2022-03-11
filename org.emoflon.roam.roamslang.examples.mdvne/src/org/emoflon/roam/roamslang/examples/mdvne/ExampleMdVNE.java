f2941b24c21666618bb39f4c898fd819056f4232
Maximilian Kratz <maximilian.kratz@es.tu-darmstadt.de>
2022-03-11 19:35:02 +0100

    Snapshot: Starts repair of MdVNE example
    This one works for now, I guess

--- org.emoflon.roam.roamslang.examples.mdvne/src/org/emoflon/roam/roamslang/examples/mdvne/ExampleMdVNE.java
+++ org.emoflon.roam.roamslang.examples.mdvne/src/org/emoflon/roam/roamslang/examples/mdvne/ExampleMdVNE.java
@@ -1,10 +1,37 @@
 package org.emoflon.roam.roamslang.examples.mdvne;
 
+import java.io.IOException;
+
+import org.eclipse.emf.common.util.URI;
+import org.emoflon.roam.core.ilp.ILPSolverOutput;
+import org.emoflon.roam.roamslang.examples.mdvne.api.roam.MdvneRoamAPI;
+
 public class ExampleMdVNE {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(final String[] args) {
+		// Create new MdVNE Roam API and load a model
+		final MdvneRoamAPI api = new MdvneRoamAPI();
+		api.init(URI.createFileURI("model.xmi"));
+		
+		// Build the ILP problem (including updates)
+		api.buildILPProblem(true);
+		final ILPSolverOutput output = api.solveILPProblem();
+		System.out.println("Solver status: " + output.status());
+		System.out.println("Objective value: " + output.objectiveValue());
+		
+		api.getSrv2srv().applyNonZeroMappings();
+		api.getSw2node().applyNonZeroMappings();
+		api.getL2p().applyNonZeroMappings();
+		api.getL2s().applyNonZeroMappings();
+		
+		try {
+			api.saveResult("model-out.xmi");
+		} catch (final IOException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println("Dat End.");
+		System.exit(0);
 	}
 
 }
