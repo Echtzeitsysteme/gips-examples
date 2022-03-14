e283f606e763b6b1c26a8575c242b24ae30c2ed4
Maximilian Kratz <maximilian.kratz@es.tu-darmstadt.de>
2022-03-14 10:13:33 +0100

    Clean up in MdVNE example
    Changes finish message

--- org.emoflon.roam.roamslang.examples.mdvne/src/org/emoflon/roam/roamslang/examples/mdvne/ExampleMdVNE.java
+++ org.emoflon.roam.roamslang.examples.mdvne/src/org/emoflon/roam/roamslang/examples/mdvne/ExampleMdVNE.java
@@ -11,26 +11,26 @@ public class ExampleMdVNE {
 	public static void main(final String[] args) {
 		// Create new MdVNE Roam API and load a model
 		final MdvneRoamAPI api = new MdvneRoamAPI();
-		api.init(URI.createFileURI("model.xmi"));
-		
+		api.init(URI.createFileURI("model-in.xmi"));
+
 		// Build the ILP problem (including updates)
 		api.buildILPProblem(true);
 		final ILPSolverOutput output = api.solveILPProblem();
 		System.out.println("Solver status: " + output.status());
 		System.out.println("Objective value: " + output.objectiveValue());
-		
+
 		api.getSrv2srv().applyNonZeroMappings();
 		api.getSw2node().applyNonZeroMappings();
 		api.getL2p().applyNonZeroMappings();
 		api.getL2s().applyNonZeroMappings();
-		
+
 		try {
 			api.saveResult("model-out.xmi");
 		} catch (final IOException e) {
 			e.printStackTrace();
 		}
-		
-		System.out.println("Dat End.");
+
+		System.out.println("Roam run finished.");
 		System.exit(0);
 	}
 
