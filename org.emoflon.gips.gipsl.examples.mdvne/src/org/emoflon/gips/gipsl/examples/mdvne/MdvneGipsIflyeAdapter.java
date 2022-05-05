e728812044f830b10b27d8e4fdee7e24748204b1
Maximilian Kratz <maximilian.kratz@es.tu-darmstadt.de>
2022-05-05 15:19:56 +0200

    Updates full MdVNE example (according to latest GIPS impl)

--- org.emoflon.gips.gipsl.examples.mdvne/src/org/emoflon/gips/gipsl/examples/mdvne/MdvneGipsIflyeAdapter.java
+++ org.emoflon.gips.gipsl.examples.mdvne/src/org/emoflon/gips/gipsl/examples/mdvne/MdvneGipsIflyeAdapter.java
@@ -49,6 +49,7 @@ public class MdvneGipsIflyeAdapter {
 		api.getSw2node().applyNonZeroMappings();
 		api.getL2p().applyNonZeroMappings();
 		api.getL2s().applyNonZeroMappings();
+		api.getNet2net().applyNonZeroMappings();
 
 		// The solution must currently be written as a file to be read by iflye
 		try {
