f28acdd67d58688c44bbb96b07630ce40ad7e60d
SebastianE <sebastian.ehmes@gmail.com>
2022-12-07 19:09:27 +0100

    + minor changes to specification + added model validator + Problem is solvable and produces a correct result!

--- PTAOptimizer/src/pta/example/house/HouseConstructionExample.java
+++ PTAOptimizer/src/pta/example/house/HouseConstructionExample.java
@@ -1,10 +1,36 @@
 package pta.example.house;
 
+import java.io.IOException;
+
+import org.eclipse.emf.common.util.URI;
+import org.emoflon.gips.core.ilp.ILPSolverOutput;
+
+import PTAOptimizer.api.gips.PTAOptimizerGipsAPI;
+
 public class HouseConstructionExample {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		PTAOptimizerGipsAPI gipsApi = new PTAOptimizerGipsAPI();
+		String projectFolder = System.getProperty("user.dir");
+		String instancesFolder = projectFolder + "/instances";
+		String file = instancesFolder + "/ConstructionProject1.xmi";
+		URI uri = URI.createFileURI(file);
+		gipsApi.init(uri);
+		
+		gipsApi.buildILPProblem(true);
+		ILPSolverOutput output = gipsApi.solveILPProblem();
+		gipsApi.getAom().applyNonZeroMappings();
+		
+		String outputFile = instancesFolder + "/ConstructionProject1_solved.xmi";
+		try {
+			gipsApi.saveResult(outputFile);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		gipsApi.terminate();
+		System.exit(0);
 	}
 
 }
