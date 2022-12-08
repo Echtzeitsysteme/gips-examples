4e7c0fa57c42853c44486e19ef26d638f2c4f1e4
SebastianE <sebastian.ehmes@gmail.com>
2022-12-08 14:56:57 +0100

    + added an example on how to use Gips and a second eMoflon-Api at the same time

--- PTAOptimizer/src/pta/example/house/HouseConstructionExample.java
+++ PTAOptimizer/src/pta/example/house/HouseConstructionExample.java
@@ -5,17 +5,29 @@ import java.io.IOException;
 import org.eclipse.emf.common.util.URI;
 import org.emoflon.gips.core.ilp.ILPSolverOutput;
 
+import PTAOptimizer.api.PTAOptimizerAPI;
+import PTAOptimizer.api.PTAOptimizerApp;
+import PTAOptimizer.api.PTAOptimizerHiPEApp;
 import PTAOptimizer.api.gips.PTAOptimizerGipsAPI;
+import PersonTaskAssignments.PersonTaskAssignmentModel;
 
 public class HouseConstructionExample {
 
 	public static void main(String[] args) {
+
+		
 		PTAOptimizerGipsAPI gipsApi = new PTAOptimizerGipsAPI();
 		String projectFolder = System.getProperty("user.dir");
 		String instancesFolder = projectFolder + "/instances";
 		String file = instancesFolder + "/ConstructionProject1.xmi";
 		URI uri = URI.createFileURI(file);
 		gipsApi.init(uri);
+		
+//		PTAOptimizerApp app = new PTAOptimizerHiPEApp();
+//		app.registerMetaModels();
+//		app.setModel(gipsApi.getEMoflonAPI().getModel());
+//		PTAOptimizerAPI api = app.initAPI();
+//		api.updateMatches();
 		
 		gipsApi.buildILPProblem(true);
 		ILPSolverOutput output = gipsApi.solveILPProblem();
