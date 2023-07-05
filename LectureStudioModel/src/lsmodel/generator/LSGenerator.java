568c748a81703f68d0698fc916017f8ae1bf6dab
SebastianE <sebastian.ehmes@gmail.com>
2023-07-05 19:44:19 +0200

    first version of the gispl specification for the p2p scenario -> Problem: Transfer time cannot be minimized since calculating transfer time requires dividing file size by connection bandwidth, which is impossible since this is a variable value. -> Any ideas?

--- LectureStudioModel/src/lsmodel/generator/LSGenerator.java
+++ LectureStudioModel/src/lsmodel/generator/LSGenerator.java
@@ -24,7 +24,7 @@ public class LSGenerator {
 	public static final DecimalFormat df = new DecimalFormat("0.00");
 
 	public static String projectFolder = System.getProperty("user.dir");
-	public static String instancesFolder = projectFolder + "/instances";
+	public static String instancesFolder = projectFolder + "/../org.emoflon.gips.gipsl.examples.lsp2p/instances/";
 	
 	protected LectureStudioModelFactory factory = LectureStudioModelFactory.eINSTANCE;
 	protected Random rnd;
@@ -57,17 +57,18 @@ public class LSGenerator {
 				new GenParameter(GenDistribution.CONST, clientsDown));
 		
 		StringBuilder fileName = new StringBuilder();
-		fileName.append("/LSBW@");
-		fileName.append(df.format(lsBW).replace(",", "-"));
-		fileName.append("_FS@");
-		fileName.append(df.format(fileSize).replace(",", "-"));
-		fileName.append("_CL@");
-		fileName.append(clients);
-		fileName.append("_CLUP@");
-		fileName.append(df.format(clientsUp).replace(",", "-"));
-		fileName.append("_CLDW@");
-		fileName.append(df.format(clientsDown).replace(",", "-"));
-		fileName.append("_EMPTY.xmi");
+//		fileName.append("/LSBW@");
+//		fileName.append(df.format(lsBW).replace(",", "-"));
+//		fileName.append("_FS@");
+//		fileName.append(df.format(fileSize).replace(",", "-"));
+//		fileName.append("_CL@");
+//		fileName.append(clients);
+//		fileName.append("_CLUP@");
+//		fileName.append(df.format(clientsUp).replace(",", "-"));
+//		fileName.append("_CLDW@");
+//		fileName.append(df.format(clientsDown).replace(",", "-"));
+//		fileName.append("_EMPTY.xmi");
+		fileName.append("lsp2p_10clients.xmi");
 
 		try {
 			save(net, instancesFolder + fileName.toString());
@@ -102,20 +103,36 @@ public class LSGenerator {
 		LectureStudioServer ls = factory.createLectureStudioServer();
 		ls.setData(data.getParam(rnd));
 		ls.setId("LS"+id++);
-		ls.setSendBandwidth(lsBwUp.getParam(rnd));
-		ls.setIsLsServer(true);
+		ls.setTxBW(lsBwUp.getParam(rnd));
+		// Rx is actually not needed.
+		ls.setRxBW(lsBwUp.getParam(rnd));
+		ls.setResidualTxBW(ls.getTxBW());
+		// Same here.
+		ls.setResidualRxBW(ls.getRxBW());
+		ls.setAllocatedTxBW(0);
+		// Same here.
+		ls.setAllocatedRxBW(0);
+		ls.setTransferTime(0);
+		ls.setIsRelayClient(1);
+		ls.setIsHasRoot(1);
 		net.getLectureStudioServer().add(ls);
 		
 		List<Client> clients = new LinkedList<>();
 		for(int i = (int)nodes.getParam(rnd); i>0; i--) {
 			Client client = factory.createClient();
-			client.setData(0.0);
+			client.setData(ls.getData());
 			client.setDepth(-1);
 			client.setId("CL"+id++);
-			client.setIsRelayClient(false);
-			client.setSendBandwidth(nodeBwUp.getParam(rnd));
-			client.setReceiveBandwidth(nodeBwDown.getParam(rnd));
-			client.setIsLsServer(false);
+			client.setIsRelayClient(0);
+			client.setTxBW(nodeBwUp.getParam(rnd));
+			client.setRxBW(nodeBwDown.getParam(rnd));
+			client.setResidualTxBW(client.getTxBW());
+			client.setResidualRxBW(client.getRxBW());
+			client.setAllocatedTxBW(0);
+			client.setAllocatedRxBW(0);
+			client.setTransferTime(0);
+			client.setIsRelayClient(0);
+			client.setIsHasRoot(0);
 			clients.add(client);
 		}
 		ls.getWaitingClients().addAll(clients);
