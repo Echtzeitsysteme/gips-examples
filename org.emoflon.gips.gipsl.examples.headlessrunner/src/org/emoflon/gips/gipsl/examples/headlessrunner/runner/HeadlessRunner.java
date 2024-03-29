1c82896321009dc7afab91dc5c2c169ebad1dacf
Maximilian Kratz <maximilian.kratz@es.tu-darmstadt.de>
2024-03-29 08:27:36 +0100

    Enforces the correct code style

--- org.emoflon.gips.gipsl.examples.headlessrunner/src/org/emoflon/gips/gipsl/examples/headlessrunner/runner/HeadlessRunner.java
+++ org.emoflon.gips.gipsl.examples.headlessrunner/src/org/emoflon/gips/gipsl/examples/headlessrunner/runner/HeadlessRunner.java
@@ -7,7 +7,8 @@ import org.emoflon.gips.gipsl.examples.headlessrunner.api.gips.HeadlessrunnerGip
 public class HeadlessRunner {
 
 	/**
-	 * Runs the headless example. First argument must be the file path for the input model to load.
+	 * Runs the headless example. First argument must be the file path for the input
+	 * model to load.
 	 * 
 	 * @param args Array of arguments.
 	 */
@@ -15,22 +16,22 @@ public class HeadlessRunner {
 		if (args == null || args.length == 0) {
 			throw new IllegalArgumentException("Arguments were null or empty.");
 		}
-		
+
 		final String arg = args[0];
 		if (arg == null || arg.isBlank()) {
 			throw new IllegalArgumentException("First argument was null or empty.");
 		}
-		
+
 		final HeadlessrunnerGipsAPI api = new HeadlessrunnerGipsAPI();
 		api.init(URI.createFileURI(arg));
-		
+
 		api.buildILPProblem(true);
 		final ILPSolverOutput output = api.solveILPProblem();
 		System.out.println("Solver status: " + output.status());
 		System.out.println("Objective value: " + output.objectiveValue());
-		
+
 		api.getZeroNode().applyNonZeroMappings();
-		
+
 		System.out.println("GIPSL run finished.");
 		System.exit(0);
 	}
