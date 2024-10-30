fe71dd6abc08c7a918e676aab4dad46c74f9de23
SebastianE <sebastian.ehmes@gmail.com>
2024-10-30 19:04:19 +0100

    updated script files

--- PTAEvaluation/scripts/test.sh
+++ PTAEvaluation/scripts/test.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
-
 set -e
-
 echo "#"
 echo "# => Test start."
 echo "#"
-
 mkdir -p ./out
-
 ./start-args-gips.sh "./RndEval1.xmi" "./out/out.xmi" "test" "BATCH-A" "./out/out.csv"
-
 echo "#"
 echo "# => Test done."
 echo "#"
