757c74e3f36157239ffecd315717d329a769737d
Maximilian Kratz <maximilian.kratz@es.tu-darmstadt.de>
2025-05-13 08:12:37 +0200

    Clones batch alt GIPSL project to create a base for the incremental impl

---
+++ teachingassistant.kcl.gipssolutionaltinc/src/teachingassistant/kcl/gipssolutionaltinc/runner/TeachingAssistantPipelineRunner.java
@@ -0,0 +1,19 @@
+package teachingassistant.kcl.gipssolutionaltinc.runner;
+
+import teachingassistant.kcl.metamodelalt.generator.SimpleTaKclGenerator;
+import teachingassistant.kcl.metamodelalt.validator.TeachingAssistantKclValidator;
+
+/**
+ * Runs the teaching assistant pipeline (scenario generator, GIPSL optimization,
+ * and validator).
+ */
+public class TeachingAssistantPipelineRunner {
+
+	public static void main(final String[] args) {
+		SimpleTaKclGenerator.main(null);
+		TeachingAssistantRunner.main(null);
+		TeachingAssistantKclValidator.main(null);
+		System.exit(0);
+	}
+
+}
