4e6585446a4b990455a88cc862d75a7306666989
Maximilian Kratz <maximilian.kratz@es.tu-darmstadt.de>
2025-05-26 21:06:05 +0200

    Snapshot: adds second swap implementation

---
+++ teachingassistant.kcl.gipssolutionaltincswap2/src/teachingassistant/kcl/gipssolutionaltincswap2/runner/TeachingAssistantPipelineRunner.java
@@ -0,0 +1,57 @@
+package teachingassistant.kcl.gipssolutionaltincswap2.runner;
+
+import teachingassistant.kcl.metamodelalt.generator.SimpleTaKclGenerator;
+import teachingassistant.kcl.metamodelalt.generator.TeachingAssistantKclManipulator;
+import teachingassistant.kcl.metamodelalt.validator.TeachingAssistantKclValidator;
+
+/**
+ * Runs the teaching assistant pipeline (scenario generator, GIPSL optimization,
+ * and validator).
+ */
+public class TeachingAssistantPipelineRunner {
+
+	public static void main(final String[] args) {
+		//
+		// Generate the initial model
+		//
+
+		SimpleTaKclGenerator.main(null);
+
+		//
+		// Optimize/solve the initial model/problem
+		//
+
+		teachingassistant.kcl.gipssolutionalt.runner.TeachingAssistantRunner.main(null);
+
+		// Validate the solution
+		TeachingAssistantKclValidator.main(null);
+
+		// Save initial solution
+		final String projectFolder = System.getProperty("user.dir");
+		final String instanceFolder = projectFolder + "/../teachingassistant.kcl.metamodelalt/instances/";
+		final String filePath = instanceFolder + TeachingAssistantKclValidator.SCENARIO_FILE_NAME;
+
+		//
+		// Alter the solution, i.e., violate a constraint by changing the model
+		//
+
+		final TeachingAssistantKclManipulator manipulator = new TeachingAssistantKclManipulator(filePath);
+		manipulator.executeBlocking();
+
+		// Model should now be invalid
+		TeachingAssistantKclValidator.main(null);
+
+		//
+		// Second stage optimization/repair
+		//
+
+		TeachingAssistantRunner.scenarioFileName = TeachingAssistantKclValidator.SCENARIO_FILE_NAME;
+		TeachingAssistantRunner.main(null);
+
+		// Validate the solution
+		TeachingAssistantKclValidator.main(null);
+
+		System.exit(0);
+	}
+
+}
---
+++ teachingassistant.kcl.gipssolutionaltincswap2/src/teachingassistant/kcl/gipssolutionaltincswap2/runner/TeachingAssistantRunner.java
@@ -0,0 +1,63 @@
+package teachingassistant.kcl.gipssolutionaltincswap2.runner;
+
+import org.eclipse.emf.common.util.URI;
+
+import teachingassistant.kcl.gipssolutionaltincswap2.api.gips.Gipssolutionaltincswap2GipsAPI;
+
+public class TeachingAssistantRunner extends AbstractTeachingAssistantRunner {
+
+	static String scenarioFileName = "kcl_ta_allocation.xmi";
+
+	public static void main(final String[] args) {
+		new TeachingAssistantRunner().run();
+	}
+
+	public void run() {
+		//
+		// Load an XMI model
+		//
+
+		final String projectFolder = System.getProperty("user.dir");
+		final String instanceFolder = projectFolder + "/../teachingassistant.kcl.metamodelalt/instances/";
+		final String filePath = instanceFolder + scenarioFileName;
+
+		checkIfFileExists(filePath);
+
+		//
+		// Initialize GIPS API
+		//
+
+		final Gipssolutionaltincswap2GipsAPI gipsApi = new Gipssolutionaltincswap2GipsAPI();
+		gipsApi.init(URI.createFileURI(filePath));
+		gipsApi.getTracer().enableTracing(true);
+		gipsApi.getEclipseIntegrationConfig().setSolutionValuesSynchronizationEnabled(true);
+
+		//
+		// Build and solve the ILP problem
+		//
+
+		final long tick = System.nanoTime();
+		buildAndSolve(gipsApi);
+		final long tock = System.nanoTime();
+
+		//
+		// Apply the solution
+		//
+
+		applySolution(gipsApi);
+
+		//
+		// Save output XMI file
+		//
+
+		gipsSave(gipsApi, instanceFolder + "solved.xmi");
+
+		//
+		// The end
+		//
+
+		System.out.println("Building + solving took " + 1.0 * (tock - tick) / 1_000_000_000 + "s.");
+		gipsApi.terminate();
+	}
+
+}
