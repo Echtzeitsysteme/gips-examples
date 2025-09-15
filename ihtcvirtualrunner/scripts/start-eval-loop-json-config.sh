e3d0723bb11a5c0fac6bb4989a319c21c021ec6e
Maximilian Kratz <maximilian.kratz@es.tu-darmstadt.de>
2025-09-15 10:08:36 +0100

    Updates runner scripts to also save the solution JSON
    ... oh boy.

--- ihtcvirtualrunner/scripts/start-eval-loop-json-config.sh
+++ ihtcvirtualrunner/scripts/start-eval-loop-json-config.sh
@@ -30,10 +30,10 @@ for ((i=1;i<=30;i++));
 do
     if [ $i -lt 10 ]; then
         echo "./ihtc2024_competition_instances/i0$i.json"
-        ./start-args-gips.sh "./ihtc2024_competition_instances/i0$i.json" 0 $callback $parameter
+        ./start-args-gips.sh "./ihtc2024_competition_instances/i0$i.json" "./ihtc2024_competition_instances/i0${i}_solution.json" 0 $callback $parameter
     else
         echo "./ihtc2024_competition_instances/i$i.json"
-        ./start-args-gips.sh "./ihtc2024_competition_instances/i$i.json" 0 $callback $parameter
+        ./start-args-gips.sh "./ihtc2024_competition_instances/i$i.json" "./ihtc2024_competition_instances/i${i}_solution.json" 0 $callback $parameter
     fi
 done
 
--- ihtcvirtualrunner/scripts/start-test-loop.sh
+++ ihtcvirtualrunner/scripts/start-test-loop.sh
@@ -24,10 +24,10 @@ for ((i=1;i<=10;i++));
 do
     if [ $i -lt 10 ]; then
         echo "./ihtc2024_test_dataset/test0$i.json"
-        ./start-args-gips.sh "./ihtc2024_test_dataset/test0$i.json"
+        ./start-args-gips.sh "./ihtc2024_test_dataset/test0$i.json" "./ihtc2024_test_dataset/test0${i}_solution.json"
     else
         echo "./ihtc2024_test_dataset/test$i.json"
-        ./start-args-gips.sh "./ihtc2024_test_dataset/test$i.json"
+        ./start-args-gips.sh "./ihtc2024_test_dataset/test$i.json" "./ihtc2024_test_dataset/test${i}_solution.json"
     fi
 done
 
