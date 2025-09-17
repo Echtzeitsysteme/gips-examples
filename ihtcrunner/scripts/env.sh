f63c13f98920a2c72928cf7122dd680fe7f426a3
Maximilian Kratz <maximilian.kratz@es.tu-darmstadt.de>
2025-09-17 14:21:59 +0100

    Updates runner scripts and config files (JSON)

--- ihtcrunner/scripts/env.sh
+++ ihtcrunner/scripts/env.sh
@@ -11,6 +11,6 @@
 #
 
 export GRB_LICENSE_FILE="/home/mkratz/gurobi.lic"
-export GUROBI_HOME="/opt/gurobi1201/linux64/"
-export LD_LIBRARY_PATH="/opt/gurobi1201/linux64/lib/"
-PATH=$PATH:~/opt/gurobi1201/linux64/bin/
+export GUROBI_HOME="/opt/gurobi1203/linux64/"
+export LD_LIBRARY_PATH="/opt/gurobi1203/linux64/lib/"
+PATH=$PATH:~/opt/gurobi1203/linux64/bin/
