0a622ba94ae9cd6a61e5e53296926c5c1c4b9489
Maximilian Kratz <maximilian.kratz@es.tu-darmstadt.de>
2025-11-20 14:13:29 +0100

    Configurable scenario file name

--- teachingassistant.uni.batch/src/teachingassistant/uni/batch/runner/TaBatchPipelineRunner.java
+++ teachingassistant.uni.batch/src/teachingassistant/uni/batch/runner/TaBatchPipelineRunner.java
@@ -9,16 +9,19 @@ import teachingassistant.uni.metamodel.validator.TeachingAssistantUniValidator;
  */
 public class TaBatchPipelineRunner {
 
+	public static String SCENARIO_FILE_NAME = "kcl_tiny_dataset";
+//	public static String SCENARIO_FILE_NAME = "kcl_semester_dataset";
+
 	public static void main(final String[] args) {
 //		SimpleTaUniGenerator.main(null);
 		final String projectFolder = System.getProperty("user.dir");
 		final String resourceFolder = projectFolder + "/../teachingassistant.uni.metamodel/resources/";
 		final String instanceFolder = projectFolder + "/../teachingassistant.uni.metamodel/instances/";
-		final String jsonFilePath = resourceFolder + "kcl_tiny_dataset.json";
-		final String xmiFilePath = instanceFolder + "kcl_tiny_dataset.xmi";
+		final String jsonFilePath = resourceFolder + SCENARIO_FILE_NAME + ".json";
+		final String xmiFilePath = instanceFolder + SCENARIO_FILE_NAME + ".xmi";
 
 		JsonToModelImporter.main(new String[] { jsonFilePath, xmiFilePath });
-		TaBatchRunner.scenarioFileName = "kcl_tiny_dataset.xmi";
+		TaBatchRunner.scenarioFileName = SCENARIO_FILE_NAME + ".xmi";
 		TaBatchRunner.main(null);
 		TeachingAssistantUniValidator.SCENARIO_FILE_NAME = "solved.xmi";
 		TeachingAssistantUniValidator.main(null);
