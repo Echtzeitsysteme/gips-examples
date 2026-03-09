d333867e86f8c433ef7d0c7c93fd4e5bc8a5af22
Maximilian Kratz <maximilian.kratz@es.tu-darmstadt.de>
2026-03-09 09:38:58 +0100

    Adds file-based export of conflicts

--- teachingassistant.uni.batch/src/teachingassistant/uni/batch/runner/TaBatchRunner.java
+++ teachingassistant.uni.batch/src/teachingassistant/uni/batch/runner/TaBatchRunner.java
@@ -1,5 +1,8 @@
 package teachingassistant.uni.batch.runner;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
@@ -18,6 +21,7 @@ import org.emoflon.gips.core.util.Observer;
 import org.emoflon.ibex.gt.api.GraphTransformationMatch;
 
 import metamodel.TaAllocation;
+import metamodel.TimeTableEntry;
 import teachingassistant.uni.batch.api.gips.BatchGipsAPI;
 import teachingassistant.uni.batch.api.gips.mapper.TaToOccurrenceMapper;
 import teachingassistant.uni.batch.api.matches.AssignTaMatch;
@@ -29,6 +33,8 @@ import teachingassistant.uni.metamodel.validator.TeachingAssistantUniValidator;
 import teachingassistant.uni.utils.AbstractGipsTeachingAssistantRunner;
 
 public class TaBatchRunner extends AbstractGipsTeachingAssistantRunner {
+
+	final String conflictWritePath = "./conflicts.txt";
 
 	public static void main(final String[] args) {
 		new TaBatchRunner().run();
@@ -161,6 +167,33 @@ public class TaBatchRunner extends AbstractGipsTeachingAssistantRunner {
 		log("Runtime GIPS save: " + tickTockToSeconds(gipsApplyDone, gipsSaveDone) + "s.");
 
 		//
+		// Print statistics about blocked dates
+		//
+
+		// conflicting entries
+		System.out.println("=> Conflicting entries stats:");
+		try {
+			final BufferedWriter writer = new BufferedWriter(new FileWriter(conflictWritePath, true));
+			printAndWrite("findConflictingEntriesWithTa:", writer);
+			gipsApi.getEMoflonAPI().findConflictingEntriesWithTa().findMatches().forEach(m -> {
+				printAndWrite("\t" + entryToString(m.getEntryA()) + ";" + entryToString(m.getEntryB()) + ";"
+						+ m.getTa().getName(), writer);
+			});
+			// inter-campus travel time
+			printAndWrite("findInterCampusTimeTableEntriesConflict:", writer);
+			gipsApi.getEMoflonAPI().findInterCampusTimeTableEntriesConflict().findMatches().forEach(m -> {
+				printAndWrite("\t" + entryToString(m.getEntryA()) + ";" + entryToString(m.getEntryB()) + ";"
+						+ m.getTa().getName(), writer);
+			});
+
+			// close file
+			writer.flush();
+			writer.close();
+		} catch (final IOException e) {
+			e.printStackTrace();
+		}
+
+		//
 		// Model Validation
 		//
 
@@ -252,6 +285,42 @@ public class TaBatchRunner extends AbstractGipsTeachingAssistantRunner {
 	 */
 	private void setGurobiVerbose(final BatchGipsAPI gipsApi, final boolean verbose) {
 		gipsApi.getSolverConfig().setEnableOutput(verbose);
+	}
+
+	/**
+	 * Converts a given time table entry to a nice printout string.
+	 * 
+	 * @param entry Time table entry to convert.
+	 * @return String representing the time table entry.
+	 */
+	private String entryToString(final TimeTableEntry entry) {
+		Objects.requireNonNull(entry);
+		String ret = "";
+		ret += entry.getSession().getName();
+		ret += "@day";
+		ret += entry.getDay();
+		ret += "@";
+		ret += entry.getRoom().getName();
+		return ret;
+	}
+
+	/**
+	 * The given String will be written to the given buffered writer and it will be
+	 * printed to the console.
+	 * 
+	 * @param value  String to be processed.
+	 * @param writer BufferedWriter to write the contents to.
+	 */
+	private void printAndWrite(final String value, final BufferedWriter writer) {
+		Objects.requireNonNull(value);
+		Objects.requireNonNull(writer);
+		System.out.println(value);
+		try {
+			writer.write(value);
+			writer.newLine();
+		} catch (final IOException ex) {
+			ex.printStackTrace();
+		}
 	}
 
 }
